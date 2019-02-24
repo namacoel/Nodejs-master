0d1d8d0d1783ff5a3a0ded4b42da720be54ad276
namacoel <namacoel@gmail.com>
2019-02-24 23:28:22 +0900

    delete 로직 개선

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ app.get('/page/:pageId', function(req, res) {
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
           <a href="/update/${sanitizedTitle}">update</a>
-          <form action="delete_process" method="post">
+          <form action="/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
@@ -76,8 +76,7 @@ app.post('/create_process', function(req, res) {
       var title = post.title;
       var description = post.description;
       fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        res.writeHead(302, {Location: `/page/${title}`});
-        res.send();
+        res.redirect(`/page/${title}`);
       })
   });
 });
@@ -120,10 +119,24 @@ app.post('/update_process', function(req, res) {
     var description = post.description;
     fs.rename(`data/${id}`, `data/${title}`, function(error){
       fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        res.writeHead(302, {Location: `/?id=${title}`});
-        res.end();
+        res.redirect(`/page/${title}`);
       })
     });
+  });
+});
+
+app.post('/delete_process', function(req, res) {
+  var body = '';
+  req.on('data', function(data) {
+      body = body + data;
+  });
+  req.on('end', function() {
+    var post = qs.parse(body);
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function(error){
+      res.redirect('/');
+    })
   });
 });
 
